7ac85065f6960adcae83786d2cbed05dfd135374
Mayur Raj <135864214+space-debris@users.noreply.github.com>
2024-02-10 15:54:06 +0000

    final

--- Array Problems/second largest and second smallest element in array.py
+++ Array Problems/second largest and second smallest element in array.py
@@ -1,13 +1,3 @@
 def getSecondOrderElements(n: int,  a: [int]) -> [int]:
-    largest=0
-    seclargest=1
-    if a[largest]>a[seclargest]:
-        pass
-    elif a[seclargest]>a[largest]:
-        largest,seclargest=seclargest,largest
-    for i in range(2,n):
-        if a[i]>a[largest]:
-            largest=i
-        elif a[i]>a[seclargest] and a[i]<a[largest]:
-            seclargest=i
-    
+    a.sort()
+    return a[-2],a[1]
