d1821492c38c41d0436a4b2a79fc1713a40898d3
Mayur Raj <135864214+space-debris@users.noreply.github.com>
2024-02-12 17:11:24 +0000

    two pointer approach

--- Array Problems/Remove Duplicates from Sorted Array.py
+++ Array Problems/Remove Duplicates from Sorted Array.py
@@ -18,3 +18,14 @@ def removeDuplicates(arr,n):
         arr[i] = unique_list[i]
 
     return i
+
+#2 ptr approach
+def removeDuplicates(arr,n):
+    i=0
+    for j in range(i+1,n):
+        if arr[j] != arr[i]:
+            arr[i+1]=arr[j]
+            i+=1
+        
+    return i+1
+#71 ms
