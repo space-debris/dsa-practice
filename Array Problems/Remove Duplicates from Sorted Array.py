2e108383de24a5ff6cf21d2f528e1bf549ef1eea
Mayur Raj <135864214+space-debris@users.noreply.github.com>
2024-02-12 16:59:44 +0000

    2nd sol

--- Array Problems/Remove Duplicates from Sorted Array.py
+++ Array Problems/Remove Duplicates from Sorted Array.py
@@ -6,6 +6,15 @@ def removeDuplicates(arr,n):
        if arr[i] not in unique:
             unique.add(arr[i])
             unique_elm+=1
-
-        
     return unique_elm
+
+def removeDuplicates(arr,n):
+    unique=set()
+    for i in range(n):
+       if arr[i] not in unique:
+            unique.add(arr[i])
+    unique_list = list(unique)
+    for i in range(len(unique_list)):
+        arr[i] = unique_list[i]
+
+    return i
